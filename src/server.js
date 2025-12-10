04daae3969b346afc8e640e5444b75cd4de16cff
ParaizØ . <eduardoparize.05@gmail.com>
2025-12-10 17:12:07 -0300

    Sistema de criação de usuarios adicionada

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import app from "./app.js";
+import connect from "./config/db.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+(async () => {
+  try {
+    await connect(process.env.MONGODB_URI);
+    app.listen(PORT, () =>
+      console.log(`API ouvindo em http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.error("Erro ao conectar no banco ou inciar o servidor", error);
+    process.exit(1);
+  }
+})();
